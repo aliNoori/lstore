5337c98c218d915f2c11ccfbb65dfebd7749296d
Ali Noori <matineali@gmail.com>
2024-11-26 23:44:49 +0330

    install socket

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -9,6 +9,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class MessageSent
 {
@@ -27,11 +28,13 @@ class MessageSent
 
     public function broadcastOn(): Channel
     {
+        Log::info('event');
         return new Channel('chat');
     }
 
     public function broadcastAs(): string
     {
+        Log::info('message.sent');
         return 'message.sent';
     }
 }
