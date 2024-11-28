e2d5e4c18c3f2d8891039f19c1bc58ef707ec0fa
Ali Noori <matineali@gmail.com>
2024-11-28 15:31:07 +0330

    install socket 37

--- routes/channels.php
+++ routes/channels.php
@@ -12,21 +12,6 @@ use Illuminate\Support\Facades\Broadcast;
 | used to check if an authenticated user can listen to the channel.
 |
 */
-\Illuminate\Support\Facades\Route::post('/broadcasting/auth', function (\Illuminate\Http\Request $request) {
-    // اطلاعات درخواست برای دیباگ
-    \Illuminate\Support\Facades\Log::info('Broadcasting auth hit', [
-        'headers' => $request->headers->all(),
-        'cookies' => $request->cookies->all(),
-        'user' => $request->user(),
-    ]);
-
-    // چک کردن کاربر احراز هویت شده و بازگشت پاسخ
-    if ($request->user()) {
-        return response()->json(['status' => 'ok']);
-    }
-
-    return response()->json(['status' => 'unauthorized'], 403);
-})->middleware('auth:sanctum');
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
