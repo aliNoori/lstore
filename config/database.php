d25051ba3c33d3d8a353be9c9f8a70dd76a2edcc
Ali Noori <matineali@gmail.com>
2024-11-29 19:13:25 +0330

    test private channel step 6

--- config/database.php
+++ config/database.php
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => null,  #env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),null, // حذف پیشوند Redis
         ],
 
         'default' => [
