aaf143be0e5d61dea6c3de95c90ad0e290b53628
Ali Noori <matineali@gmail.com>
2024-11-29 18:46:09 +0330

    test private channel step 4

--- routes/channels.php
+++ routes/channels.php
@@ -19,7 +19,7 @@ Broadcast::channel('chat', function ($user) {
     \Illuminate\Support\Facades\Log::info('CHANNEL',[$user]);
     return true; // دسترسی باز
 });
-Broadcast::channel('private-order-{orderId}', function ($user,$orderId) {
+Broadcast::channel('private-laravel_database_private-order-{orderId}', function ($user,$orderId) {
     \Illuminate\Support\Facades\Log::info('PrivateCHANNEL',[$orderId]);
     return true; // دسترسی باز
 });
