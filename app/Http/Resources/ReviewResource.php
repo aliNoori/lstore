d76817bf6cc0f6bf4e7053fefb2ce275bd70d25d
Ali Noori <matineali@gmail.com>
2025-02-09 22:44:58 +0330

    add user reviews to resource

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ReviewResource extends JsonResource
@@ -10,14 +11,16 @@ class ReviewResource extends JsonResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @return array|Arrayable|\JsonSerializable
      */
-    public function toArray($request)
+    public function toArray($request): array|\JsonSerializable|Arrayable
     {
         //return parent::toArray($request);
         return [
             'rating'=>$this->rating,
-            'review'=>$this->review
+            'review'=>$this->review,
+            'user' => new UserResource($this->whenLoaded('user')),
+            //'product' => new UserResource($this->whenLoaded('product'))
         ];
     }
 }
